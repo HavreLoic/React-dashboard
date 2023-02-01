390f351ffcf579ee57f6fab8fedd2e15af2badce
Havre Loic <dongmoabenahavreloic030@gmail.com>
2023-02-01 22:34:20 +0100

    Fix Syncfusion. Add line chart and bar chart showing Budget and expense with tooltip

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,7 +1,13 @@
 
-const Button = () => {
+const Button = ({ color, backgroundColor, text, size, borderRadius }) => {
     return (
-        <div>Button</div>
+        <button
+            type="button"
+            style={{ backgroundColor: backgroundColor, color, borderRadius }}
+            className={`text-${size} p-3 hover:drop-shadow-xl`}
+        >
+            {text}
+        </button>
     )
 }
 
--- src/pages/charts/stacked.jsx
+++ src/pages/charts/stacked.jsx
@@ -1,7 +1,26 @@
+import React from "react";
+import { ChartComponent, SeriesCollectionDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from "@syncfusion/ej2-react-charts";
+import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from "../../data/dummy";
+import { StackedColumnDirective } from "@syncfusion/ej2-react-grids";
+import { SeriesDirective } from "@syncfusion/ej2-react-charts/src/chart";
 
-const Stacked = () => {
+const Stacked = ({ width, height }) => {
     return (
-        <div>Stacked</div>
+        <ChartComponent
+            width={width}
+            height={height}
+            id="charts"
+            primaryXAxis={stackedPrimaryXAxis}
+            primaryYAxis={stackedPrimaryYAxis}
+            chartArea={{ border: { width: 0 } }}
+            tooltip={{ enable: true }}
+            legendSettings={{ background: 'white' }}
+        >
+            <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+            <SeriesCollectionDirective>
+                {stackedCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
+            </SeriesCollectionDirective>
+        </ChartComponent>
     )
 }
 
